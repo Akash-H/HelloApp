e615bb7de8dd773b4d3c39a5d9ab7e3153b9ea03
Akash <akash_hattargi@persistent.com>
2019-08-21 16:05:37 +0530

    new changes

--- src/app/app2.java
+++ src/app/app2.java
@@ -3,6 +3,6 @@ package app;
 class App2{
     public static void main(String[] args)
     {
-        System.out.println("How are you ?");
+        System.out.println("How are you?");
     }
 }
--- src/bean/bean.java
+++ src/bean/bean.java
@@ -3,7 +3,7 @@ package app;
 class Bean{
     public static void main(String[] args)
     {
-        System.out.println("Hello");
+        System.out.println("Hey");
         System.out.println("How Are You ?");
     }
 }
